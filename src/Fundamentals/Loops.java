629b0cfe296f2ef89514fbde13422efce27073e4
Rishon Kumar Rahi <rishon.kumar.rahi@gmail.com>
2021-09-24 01:20:13 +0530

    Loops

--- src/Fundamentals/Loops.java
+++ src/Fundamentals/Loops.java
@@ -1,4 +1,45 @@
 package Fundamentals;
 
 public class Loops {
+
+    public static void main(String[] args) {
+
+        //while loop
+        /*
+            while(condition){
+                //body
+             }
+         */
+        int count = 1;
+        while (count != 5) {
+            System.out.println("Using while loop " + count);
+            count++;
+        }
+        // If u don't know how many times a loop will run then use while loop
+
+        //for loop
+
+        /*
+            for(initialisation; condtion; increment/decrement)
+                //body
+         */
+
+        for (int countF = 1; countF != 5; countF++) {
+            System.out.println(countF);
+        }
+
+        // do-while
+        /*
+           do{
+              body
+           }while(condition)
+         */
+        //LOOP will excute atleast once
+        int number = 1;
+        do {
+            System.out.println(number);
+            number++;
+        } while (number <= 5);
+    }
+
 }
