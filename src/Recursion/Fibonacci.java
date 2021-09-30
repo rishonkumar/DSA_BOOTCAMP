cbe2dba1e471120a1c6b595e0f554f75ece4dc7c
Rishon Kumar Rahi <rishon.kumar.rahi@gmail.com>
2021-10-01 00:25:18 +0530

    Fibonacci

---
+++ src/Recursion/Fibonacci.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        int ans = 4;
+        System.out.println(fibo(ans));
+    }
+
+    static int fibo(int n) {
+        // base condition
+
+        if (n < 2) {
+            return n;
+        }
+
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
